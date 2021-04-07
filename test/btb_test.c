f8474ddcc5ea0c17a4346da38e95fd7b98d2e8b4
ceccoemi <cecchini.mle@gmail.com>
2021-04-07 18:41:15 +0200

    Add test int and string

--- test/btb_test.c
+++ test/btb_test.c
@@ -153,6 +153,80 @@ void test_tokenize_str(void)
   free_tokenizer(t);
 }
 
+void test_tokenize_int_str(void)
+{
+  char input_str[] = "i34e10:holaholabb";
+  tokenizer *t = init_tokenizer(input_str);
+
+  char *want = malloc(strlen(input_str) + 1);
+  int err;
+
+  err = next(t);
+  if (err != TOKENIZER_OK) {
+    fprintf(stderr, "next 1 failed: got %d error code\n", err);
+    return;
+  }
+  strcpy(want, "i");
+  if (strcmp(t->token, want) != 0) {
+    fprintf(stderr, "wrong 1 token: got %s, want %s\n", t->token, want);
+    return;
+  }
+
+  err = next(t);
+  if (err != TOKENIZER_OK) {
+    fprintf(stderr, "next 2 failed: got %d error code\n", err);
+    return;
+  }
+  strcpy(want, "34");
+  if (strcmp(t->token, want) != 0) {
+    fprintf(stderr, "wrong 2 token: got %s, want %s\n", t->token, want);
+    return;
+  }
+
+  err = next(t);
+  if (err != TOKENIZER_OK) {
+    fprintf(stderr, "next 3 failed: got %d error code\n", err);
+    return;
+  }
+  strcpy(want, "e");
+  if (strcmp(t->token, want) != 0) {
+    fprintf(stderr, "wrong 3 token: got %s, want %s\n", t->token, want);
+    return;
+  }
+
+  err = next(t);
+  if (err != TOKENIZER_OK) {
+    fprintf(stderr, "next 4 failed: got %d error code\n", err);
+    return;
+  }
+  strcpy(want, "s");
+  if (strcmp(t->token, want) != 0) {
+    fprintf(stderr, "wrong 4 token: got %s, want %s\n", t->token, want);
+    return;
+  }
+
+  err = next(t);
+  if (err != TOKENIZER_OK) {
+    fprintf(stderr, "next 5 failed: got %d error code\n", err);
+    return;
+  }
+  strcpy(want, "holaholabb");
+  if (strcmp(t->token, want) != 0) {
+    fprintf(stderr, "wrong 5 token: got %s, want %s\n", t->token, want);
+    return;
+  }
+
+  err = next(t);
+  if (err != TOKENIZER_END) {
+    fprintf(stderr, "next 6 failed: got %d error code, want %d\n", err,
+            TOKENIZER_END);
+    return;
+  }
+
+  free(want);
+  free_tokenizer(t);
+}
+
 int main(void)
 {
   fprintf(stdout, "Running tests...\n");
@@ -160,5 +234,7 @@ int main(void)
   test_tokenize_int();
   fprintf(stdout, "\ttest_tokenize_str\n");
   test_tokenize_str();
+  fprintf(stdout, "\ttest_tokenize_int_str\n");
+  test_tokenize_int_str();
   fprintf(stdout, "done");
 }
