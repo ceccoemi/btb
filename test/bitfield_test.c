db2bda04f097cbc82e76d76ec92eff8577785c6d
ceccoemi <cecchini.mle@gmail.com>
2021-05-21 07:13:34 +0200

    Implemented bitfield

--- test/bitfield_test.c
+++ test/bitfield_test.c
@@ -10,41 +10,38 @@
 
 void test_bitfield()
 {
-  torrent_file *tf = init_torrent_file();
-  int err = parse_torrent_file(tf, "test/data/debian-10.9.0-amd64-netinst.iso.torrent");
-  if (err != TORRENT_OK) {
-    fprintf(stderr, "parsing failed: got error code %d\n", err);
-    goto exit;
-  }
-  char peer_id[PEER_ID_LENGTH + 1] = "BTB-14011996ecis2211";
-  tracker_response *r = contact_tracker(tf, peer_id);
-  if (r == NULL) {
-    fprintf(stderr, "failed to contact the tracker");
-    goto exit;
-  }
-  handshake_msg *h = init_handshake_msg(peer_id, tf->info_hash);
-  for (long i = 0; i < r->num_peers && i < 10; i++) {
-    int sockfd = perform_handshake(r->peers[i], h);
-    if (sockfd < 0) continue;
-    message *m = read_message(sockfd);
-    if (m == NULL) continue;
-    if (m->id == MSG_BITFIELD) {
-      bitfield *b = init_bitfield(m->payload, m->payload_len);
-      for (long long k = 0; k < tf->num_pieces; k += 8) {
-        fprintf(stdout, "bitfield byte value: %i\n", b->data[k]);
-        for (int j = k; j < k + 8; j++) {
-          if (has_piece(b, j)) {
-            fprintf(stdout, "has piece %d\n", j);
-          }
-        }
-      }
-      free_bitfield(b);
-      break;
+  unsigned char data[2] = {10, 33};  // 00001010 00100001
+  bitfield *b = init_bitfield(data, 2);
+  for (int i = 0; i < 4; i++) {
+    if (has_piece(b, i)) {
+      fprintf(stderr, "expected not having %d-th piece\n", i);
     }
   }
-  free_handshake_msg(h);
-
-exit:
-  free_torrent_file(tf);
-  free_tracker_response(r);
+  if (!has_piece(b, 4)) {
+    fprintf(stderr, "should have %d-th piece\n", 4);
+  }
+  if (has_piece(b, 5)) {
+    fprintf(stderr, "expected not having %d-th piece\n", 5);
+  }
+  if (!has_piece(b, 6)) {
+    fprintf(stderr, "should have %d-th piece\n", 6);
+  }
+  if (has_piece(b, 7)) {
+    fprintf(stderr, "expected not having %d-th piece\n", 7);
+  }
+  for (int i = 8; i < 10; i++) {
+    if (has_piece(b, i)) {
+      fprintf(stderr, "expected not having %d-th piece\n", i);
+    }
+  }
+  if (!has_piece(b, 10)) {
+    fprintf(stderr, "should have %d-th piece\n", 10);
+  }
+  if (has_piece(b, 12)) {
+    fprintf(stderr, "expected not having %d-th piece\n", 12);
+  }
+  if (!has_piece(b, 15)) {
+    fprintf(stderr, "should have %d-th piece\n", 15);
+  }
+  fprintf(stdout, "%d %d\n", data[0], data[1]);
 }
